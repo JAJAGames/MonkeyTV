049bbd676b811353f0f475811402f3e7e6610958
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-03-23 12:47:50 +0100

    Added comments to script
    Menu click script with header and comments

---
+++ Assets/Scripts/Menu/MenuClick.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+/* MENUCLICK.CS
+ * (C) COPYRIGHT "JAJA GAMES", 2.016
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * EXPLANATION: 
+ * EVENTS AND ACTIONS SCRIPT OF MENU BUTTONS  
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * FUNCTIONS LIST:
+ * 
+ * LoadScene 	(int)
+ * ToggleCredits()
+ * ToggleOptions()
+ * CloseGame	()
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * MODIFICATIONS:
+ * DATA			DESCRIPCTION
+ * ----------	-----------------------------------------------------------------------------------------------------------------------
+ * 23/03/2016	CODE BASE MATCHED TO MENU BUTTONS OF THE MENU SCENE
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ */
+
+public class MenuClick : MonoBehaviour {
+
+	// 3 MENU PANELS WITCH WE CAN TOGGLE 
+	public GameObject panelMenu;
+	public GameObject panelCredits;
+	public GameObject panelOptions;
+
+	//CHANGE TO NEW SCENE. IN INSPECTOR WE CAN SET THE BUILD INDEX OF THE NEW SCENE.
+	public void LoadScene(int level)
+	{
+		SceneManager.LoadScene(level);
+	}
+
+	//TOGGLE PANEL MENU WITH PANEL CREDITS AND BACK
+	public void ToggleCredits()
+	{
+		panelMenu.SetActive (!panelMenu.activeSelf);
+		panelCredits.SetActive (!panelCredits.activeSelf);
+	}
+
+	//TOGGLE PANEL MENU WITH PANEL OPTIONS AND BACK
+	public void ToggleOptions()
+	{
+		panelMenu.SetActive (!panelMenu.activeSelf);
+		panelOptions.SetActive (!panelOptions.activeSelf);
+	}
+
+	//CLOSE GAME
+	public void CloseGame()
+	{
+		Application.Quit ();
+	}
+}
