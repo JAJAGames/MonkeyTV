162b9111e539d87302b2cf35d286a581b4c461f4
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-03-23 12:15:37 +0100

    menu options
    Simple set up menu.

---
+++ Assets/Scripts/MenuClick.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class MenuClick : MonoBehaviour {
+
+	public GameObject panelMenu;
+	public GameObject panelCredits;
+	public GameObject panelOptions;
+
+	public void LoadScene(int level)
+	{
+		SceneManager.LoadScene(level);
+	}
+
+
+	public void ToggleCredits()
+	{
+		panelMenu.SetActive (!panelMenu.activeSelf);
+		panelCredits.SetActive (!panelCredits.activeSelf);
+	}
+
+	public void ToggleOptions()
+	{
+		panelMenu.SetActive (!panelMenu.activeSelf);
+		panelOptions.SetActive (!panelOptions.activeSelf);
+	}
+
+	public void CloseGame()
+	{
+		Application.Quit ();
+	}
+}
