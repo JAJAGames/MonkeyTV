50c8c608bec21d44649eb8ca99dab230e6b76abd
AntoniGari <slemonboy@hotmail.com>
2016-03-23 13:00:54 +0100

    First commit
    Scripts done:
    CharacterMovement
    CameraController
    
    Added Level1 with colliders

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* CAMERACONTROLLER.CS
+ * (C) COPYRIGHT "JAJA GAMES", 2.016
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * EXPLANATION: 
+ * Move the camera relative to the player.
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * FUNCTIONS LIST:
+ * 
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * MODIFICATIONS:
+ * DATA			DESCRIPCTION
+ * ----------	-----------------------------------------------------------------------------------------------------------------------
+ * XX/XX/XXXX	XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ */
+
+public class CameraController : MonoBehaviour {
+
+	public PlayerMovement Player;
+	private Vector3 mPreviousPosition;
+
+	private void Awake() {
+		mPreviousPosition = Player.transform.position;
+	}
+
+	private void LateUpdate() {
+		if (!Player.IsMoving)
+			return;
+
+		Vector3 difference = Player.transform.position - mPreviousPosition;
+
+		transform.position += difference;
+
+		mPreviousPosition = Player.transform.position;
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* PLAYERMOVEMENT.CS
+ * (C) COPYRIGHT "JAJA GAMES", 2.016
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * EXPLANATION: 
+ * Control the player movement
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * FUNCTIONS LIST:
+ * 
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * MODIFICATIONS:
+ * DATA			DESCRIPCTION
+ * ----------	-----------------------------------------------------------------------------------------------------------------------
+ * XX/XX/XXXX	XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ */
+
+public class PlayerMovement : MonoBehaviour  {
+
+	public bool IsDead = false;
+
+	public float MovementSpeed = 1;
+	public float RotationSpeed = 1;
+
+
+	/* NEW */
+	public float jumpSpeed = 8.0f;
+	public float gravity = 20.0f;
+
+	Vector3 direction;
+
+	public bool IsMoving {
+		get {
+			return !IsDead && (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0);
+		}
+	}
+
+	private CharacterController mController;
+
+	public void KillPlayer() {
+		if (IsDead)
+			return;
+
+		StartCoroutine (RestartGame ());
+
+		IsDead = true;
+	}
+		
+	private void Awake ()  {
+		mController = GetComponent<CharacterController> ();
+	}
+
+	private void Update ()  {
+		if (IsDead)
+			return;
+		
+		if (mController.isGrounded) {
+			direction = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+
+			if (Input.GetButton("Jump"))
+				direction.y = jumpSpeed;
+		}
+
+		if (direction == Vector3.zero)
+			return;
+		transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation (direction), Time.deltaTime * RotationSpeed);
+
+		//direction.y -= gravity * Time.deltaTime;
+
+		mController.SimpleMove (direction * MovementSpeed);
+	}
+
+	private IEnumerator RestartGame() {
+		yield return new WaitForSeconds (3);
+		Application.LoadLevel (0);
+	}
+}
