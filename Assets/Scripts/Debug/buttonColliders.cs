11c39f1bc795fecd5c05a3d3465b14ce5912c3e5
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-03-28 11:19:53 +0200

    show/hide colliders
    button show/hide colliders

--- Assets/Scripts/Debug/buttonColliders.cs
+++ Assets/Scripts/Debug/buttonColliders.cs
@@ -5,70 +5,67 @@ using System.Collections;
 public class buttonColliders : MonoBehaviour {
 
 	// Use this for initialization
-	[Header("walls")]
-	[Tooltip("Parent of walls renders")]
+	[Header("NivelMCS01")]
+	public Transform NivelMCS01;
+	private GameObject [] rendersMCS01;
 
-	public Transform walls;
-	private GameObject [] wallsRenders;
-	[Tooltip("Collider of walls")]
-	public GameObject wallsCollider;
+	[Header("NivelMCS02")]
+	public Transform NivelMCS02;
+	private  GameObject[] rendersMCS02;
 
-	[Header("furniture")]
-	public Transform furniture;
-	private  GameObject[] furnitureRenders;
-	public GameObject furnitureCollider;
+	[Header("NivelMCS03")]
+	public Transform NivelMCS03;
+	private  GameObject[] rendersMCS03;
 
-	[Header("ground")]
-	public GameObject groundRender;
-	public GameObject groundCollider;
-	[Header ("background")]
-	public GameObject background;
+	[Tooltip("Colliders")]
+	public Transform colliders;
+	private  GameObject[] colliderRenders;
+
 	[Header("shader")]
 	public Shader wireframe;
 	Shader transparent;
 
 	void Awake () {
 		//wallsRenders = new List<GameObject>();
-		wallsRenders		= HelperMethods.GetChildren(walls);
-		furnitureRenders 	= HelperMethods.GetChildren(furniture);
+		rendersMCS01		= HelperMethods.GetChildren(NivelMCS01);
+		rendersMCS02 		= HelperMethods.GetChildren(NivelMCS02);
+		rendersMCS03 		= HelperMethods.GetChildren(NivelMCS03);
+		colliderRenders 	= HelperMethods.GetChildren (colliders);
 
 		//show renders
-		groundRender.SetActive (true);
-		background.SetActive (true);
-		for (int i = 0; i < wallsRenders.Length; i++)
-			wallsRenders [i].SetActive (true);
-		for (int i = 0; i < furnitureRenders.Length; i++)
-			furnitureRenders [i].SetActive (true);
-
-		//hide colliders
-		groundCollider.GetComponent<MeshRenderer> ().enabled = false;
-		wallsCollider.GetComponent<MeshRenderer>().enabled = false;
-		furnitureCollider.GetComponent<MeshRenderer>().enabled = false;
-
-		//set wireframe mode
-		furnitureCollider.GetComponent<MeshRenderer> ().material.shader = Shader.Find("Transparent/Diffuse");
-		wallsCollider.GetComponent<MeshRenderer> ().material.shader = Shader.Find("Transparent/Diffuse");
+		for (int i = 0; i < rendersMCS01.Length; i++)
+			rendersMCS01 [i].SetActive (true);
+		for (int i = 0; i < rendersMCS02.Length; i++)
+			rendersMCS02 [i].SetActive (true);
+		for (int i = 0; i < rendersMCS03.Length; i++)
+			rendersMCS03 [i].SetActive (true);
+		
+		//set wireframe mode and hide colliders
+		for (int i = 0; i < colliderRenders.Length; i++) 
+		{
+			colliderRenders[i].GetComponent<MeshRenderer> ().material.shader = Shader.Find ("Transparent/Diffuse");
+			colliderRenders[i].GetComponent<MeshRenderer> ().enabled = false;
+		}
 	}
 		
 
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyUp(KeyCode.F11))
-		{
-			//show&hide render
-			groundRender.SetActive (!groundRender.activeSelf);
-			for (int i = 0; i < wallsRenders.Length; i++)
-				wallsRenders [i].SetActive (!wallsRenders [i].activeSelf);
-			for (int i = 0; i < furnitureRenders.Length; i++)
-				furnitureRenders [i].SetActive (!furnitureRenders [i].activeSelf);
+	public void ButtonCollidersPressed () 
+	{
+		//show&hide render
+		for (int i = 0; i < rendersMCS01.Length; i++)
+			rendersMCS01 [i].SetActive (!rendersMCS01 [i].activeSelf);
+		for (int i = 0; i < rendersMCS02.Length; i++)
+			rendersMCS02 [i].SetActive (!rendersMCS02 [i].activeSelf);
+		for (int i = 0; i < rendersMCS03.Length; i++)
+			rendersMCS03 [i].SetActive (!rendersMCS03 [i].activeSelf);
+		for (int i = 0; i < colliderRenders.Length; i++) 
+			colliderRenders[i].GetComponent<MeshRenderer> ().enabled = !colliderRenders[i].GetComponent<MeshRenderer> ().enabled;
 
-			//show&hide colliders
-			groundCollider.GetComponent<MeshRenderer> ().enabled = !groundCollider.GetComponent<MeshRenderer> ().enabled;
-			wallsCollider.GetComponent<MeshRenderer>().enabled = !wallsCollider.GetComponent<MeshRenderer>().enabled;
-			furnitureCollider.GetComponent<MeshRenderer>().enabled = !furnitureCollider.GetComponent<MeshRenderer>().enabled;
-			background.SetActive (!background.activeSelf);
-		}
+	}
+}
 
+		/*
 		if (Input.GetKeyUp (KeyCode.F12)) 
 		{
 
@@ -83,6 +80,5 @@ public class buttonColliders : MonoBehaviour {
 			}
 
 		}
+		*/
 
-	}
-}
