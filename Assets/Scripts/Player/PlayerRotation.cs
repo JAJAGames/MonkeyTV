6e4fce5c21adf8e13fe72aa9ce08e2f8cdd56d6d
AntoniGari <slemonboy@hotmail.com>
2016-03-28 12:14:36 +0200

    Player Rotation
    - Player rotates toward mouse direction
    - Changed Gorilla01 prefab

---
+++ Assets/Scripts/Player/PlayerRotation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRotation : MonoBehaviour {
+
+	private int floorMask;
+	private float camRayLength = 100f;
+
+	private void Awake ()  {
+		floorMask = LayerMask.GetMask ("Floor");
+	}
+
+	void Update () {
+		// get the mouse point in background plane
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit floorHit;
+		Physics.Raycast (camRay, out floorHit, camRayLength, floorMask);
+
+		// look to point
+		Vector3 lookTo = floorHit.point;
+		lookTo.y = transform.position.y;
+		transform.LookAt(lookTo);
+	}
+}
