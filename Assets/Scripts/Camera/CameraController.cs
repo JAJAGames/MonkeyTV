984ea85a21bb264a6a40aa211a08952286d26179
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-03-30 13:19:11 +0200

    camera follow
    camera never follow player while jump.
    
    If player goes out of screen the camera reset their position to player
    position plus initial offset.

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -14,55 +14,76 @@ using System.Collections;
  * DATA			DESCRIPCTION
  * ----------	-----------------------------------------------------------------------------------------------------------------------
  * XX/XX/XXXX	XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
- * 30/03/2016   Static camera while player jump and fall at same place.
- * 30/03/2016	Don't follow player while is not grounded. And old method comented
+ * 30/03/2016   ----- OLD METHOD -------------------> Static camera while player jump and fall at same place.
+ * 30/03/2016	----- NEVER FOLLOW PLAYER JUMPING --> camera never follow player while jump. If player goes out of screen the camera 
+ * 				reset their position to player position plus initial offset.
  * ------------------------------------------------------------------------------------------------------------------------------------
  */
 
 public class CameraController : MonoBehaviour {
 
-	public PlayerMovement Player;
-	//private Vector3 mPreviousPosition;
-	private Vector3 initDifference;
-	private float smooth = 10;
+	public PlayerMovement playerMove; 	//used in both methods
 
-	//Don't follow while jump
+	private Vector3 initDifference;
+	private float smooth;
+	private bool playerVisible;
+
+	//NEVER FOLLOW PLAYER JUMPING
+
 	private void Start() 
 	{
-		initDifference = transform.position - Player.transform.position;
+		initDifference = transform.position - playerMove.transform.position; 				//get the initial offset
+		smooth = playerMove.gameObject.GetComponent<PlayerMovement>().movementSpeed * 3;	//get the speed of camera. It should be faster than player
+		playerVisible = true;																//in initial offset the player is visible
 	}
 
 	private void LateUpdate() {
+		 
+		//get the position of player in screen
+		Vector3 screenPoint = Camera.main.WorldToViewportPoint (playerMove.transform.position); 
 
-		if (!Player.controller.isGrounded)
+		//is visible?
+		if (screenPoint.z > 0.1 && screenPoint.x > 0.1 && screenPoint.x < 0.9 && screenPoint.y > 0.1 && screenPoint.y < 0.9)
+			playerVisible = true;
+		else
+			playerVisible = false;
+
+		//visible and jumping the camera remains in same place --> stop update
+		if (!playerMove.controller.isGrounded && playerVisible)
 			return;
-	
-		Vector3 difference = transform.position - Player.transform.position;
 
-		if (difference != initDifference)
-			transform.position = Vector3.MoveTowards (transform.position, Player.transform.position + initDifference, smooth * Time.deltaTime);
+		// current offset
+		Vector3 difference = transform.position - playerMove.transform.position;
 
+		//player visible and not jumping -> move camera.
+		if (difference != initDifference && playerVisible)
+			transform.position = Vector3.MoveTowards (transform.position, playerMove.transform.position + initDifference, smooth * Time.deltaTime);
+		
+		//is out of screen set position with initial offset to camera
+		if (!playerVisible)
+			transform.position =  playerMove.transform.position + initDifference;
 	}
 
 
 /*----------------------------------------------------------------- OLD METHOD ------		
  * follow while jump but stay on place if jumps and falls at same place
 
+//	private Vector3 mPreviousPosition;
 
 	private void Awake() {
-		mPreviousPosition = Player.transform.position;
+		mPreviousPosition = playerMove.transform.position;
 	}
 
 	private void LateUpdate() {
 
-		Vector3 difference = Player.transform.position - mPreviousPosition;
+		Vector3 difference = playerMove.transform.position - mPreviousPosition;
 
 		//the camera remain in place when player jumps without forward direction
 		if (!Player.controller.isGrounded && difference.x == 0 && difference.z == 0) 	
 			return;
 
 		transform.position += difference;
-		mPreviousPosition = Player.transform.position;
+		mPreviousPosition = playerMove.transform.position;
 	}
 */
 
