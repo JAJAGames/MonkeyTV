f4146f3c58d5a75f4a995b1873e8a35f92615c9d
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-04-06 11:27:36 +0200

    Player modification
    Added ParticleSystem to player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -13,7 +13,7 @@ using System.Collections;
  * MODIFICATIONS:
  * DATA			DESCRIPCTION
  * ----------	-----------------------------------------------------------------------------------------------------------------------
- * XX/XX/XXXX	XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+ * 06/04/2016	Added ParticleSystem to player. The new code allow to enable and disable emitter 
  * ------------------------------------------------------------------------------------------------------------------------------------
  */
 
@@ -26,25 +26,42 @@ public class PlayerMovement : MonoBehaviour  {
 
 	private Vector3 moveDirection = Vector3.zero;
 	public CharacterController controller;
-
+	public ParticleSystem walkParticles;
+	ParticleSystem.EmissionModule em;
 	private void Awake ()  {
 		controller = GetComponent<CharacterController> ();
+		em = walkParticles.emission;
+		em.enabled = false;
 	}
 
 	private void Update ()  {
+																			//no update for deads... no Zombies please!
 		if (IsDead)
 			return;
-
+		
+																			//if grounded get input
 		if (controller.isGrounded) {
+			
 			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 			moveDirection = transform.TransformDirection(moveDirection);
 			moveDirection *= movementSpeed;
 
-			if (Input.GetButton("Jump"))
+			if (moveDirection != Vector3.zero) 								//enable or disable emitter of ParticleSystem
+			{
+				em.enabled = true;
+			}
+			else
+				em.enabled = false;
+
+			if (Input.GetButton ("Jump")) 									//jump go up y axis!! and no particles...
+			{
 				moveDirection.y = jumpSpeed;
+				em.enabled = false;
+			} 
+
 		}
 
-		moveDirection.y -= gravity * Time.deltaTime;
+		moveDirection.y -= gravity * Time.deltaTime;						//calculate translation
 		controller.Move(moveDirection * Time.deltaTime);
 	}
 }
