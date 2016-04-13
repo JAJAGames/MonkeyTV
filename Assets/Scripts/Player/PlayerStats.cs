5f02d4fa8de95aa786b31f4cc8042d939cc9fae4
AntoniGari <slemonboy@hotmail.com>
2016-04-13 10:32:34 +0200

    Scene modified and PlayerStats Added
    - Added PlayerStats Script
    - Scene Level1_Angel modified (added colliders and renders to PanelDebug
    Script

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,48 @@
+﻿/* PLAYERSTATS.CS
+ * (C) COPYRIGHT "JAJA GAMES", 2.016
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * EXPLANATION: 
+ * Manages the next player Stats:
+ * - Health
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * FUNCTIONS LIST:
+ * Awake		()
+ * TakeDamage	(int)
+ * Death		()
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * MODIFICATIONS:
+ * DATA			DESCRIPCTION
+ * ----------	-----------------------------------------------------------------------------------------------------------------------
+ * XX/XX/XXXX	XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	public int startingHealth = 4;            
+	public int currentHealth;
+	public bool isDead;
+
+	void Awake () {
+		currentHealth = startingHealth;
+	}
+
+	public void TakeDamage (int damage) {
+		if(isDead)
+			return;
+
+		currentHealth -= damage;
+
+		if(currentHealth <= 0) {
+			StartCoroutine (Death());
+		}
+	}
+
+	private IEnumerator Death () {
+		isDead = true;
+		yield return new WaitForSeconds(1.0f);
+	}
+}
