314feed324ee7a076e631c6784ec5be75f678dd0
Jesús Mosquera Mayo <jesusmm12@TTC.CAT>
2016-04-14 20:54:06 +0200

    camera targettin
    TODO follow player and spawn cubes to floor.

---
+++ Assets/Scripts/Props/Targetting.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Targetting : MonoBehaviour {
+
+    public Transform player;
+	// Update is called once per frame
+	void Update () {
+        Vector3 lookAt = player.position;
+        lookAt.y = transform.position.y;
+        transform.LookAt(lookAt);
+	}
+}
