05ea4d32d4dadb388eb908174ee337aeb0d92ce5
Jesús Mosquera Mayo <jesusmm12@TTC.CAT>
2016-04-18 18:37:25 +0200

    main scene actualized
    added shoots to enemies in main scene

--- Assets/Scripts/Languages/XMLSerialize/LanguageXMLSerializer.cs
+++ Assets/Scripts/Languages/XMLSerialize/LanguageXMLSerializer.cs
@@ -26,30 +26,3 @@ public class LanguageXMLSerializer {
 			return serializer.Deserialize(stream) as LanguageXMLSerializer;
 	}
 }
-
-/*
-	public void Save (string path)
-	{
-		var serializer = new XmlSerializer (typeof(LanguageXMLSerializer));
-		var stream = new FileStream (path, FileMode.Create);
-		// Instantiate xmlWriter and pass stream and encoding.
-		XmlWriter writer = new XmlTextWriter(stream, Encoding.Unicode);
-		serializer.Serialize (writer, this);
-	}
-	
-
-public static LanguageXMLSerializer Load (string path)
-{
-	// Instantiate xmlSerializer with my object type
-	var serializer = new XmlSerializer (typeof(LanguageXMLSerializer));
-
-	// Instantiate a new stream and pass file location and mode.
-	Stream stream = new FileStream(path, FileMode.Open);
-
-
-	// Instantiate xmlWriter and pass stream and encoding.
-	XmlWriter xmlWriter = new XmlTextWriter(stream, Encoding.Unicode);
-
-	// Call serialize method and pass xmlWriter and my object.
-	return serializer.Deserialize(stream) as LanguageXMLSerializer;
-}*/
