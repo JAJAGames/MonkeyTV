16b13a954968a2da258c23b9a89ab033937576ec
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-04-20 11:47:22 +0200

    battery prop
    1. Added behavior to battery prop
    2. added rigid body to playershoot bullets
    3. added torus fix to scene
    4. modified mode god

--- Assets/Scripts/PoolManager/Test/PlayerShootObject.cs
+++ Assets/Scripts/PoolManager/Test/PlayerShootObject.cs
@@ -1,16 +1,41 @@
-﻿using UnityEngine;
+﻿/* PLAYERSHOOTOBJECT.CS
+ * (C) COPYRIGHT "JAJA GAMES", 2.016
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * EXPLANATION: 
+ * This script allows the behaviour of player bullets
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * FUNCTIONS LIST:
+ * 
+ * Awake ()
+ * Update ()
+ * OntriggerStay ()
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * MODIFICATIONS:
+ * DATA			DESCRIPCTION
+ * ----------	-----------------------------------------------------------------------------------------------------------------------
+ * 28/03/2016	GENERIC METHODS TO USE IN SCRIPTS
+ * 20/04/2016	Added playershot tag
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ */
+
+using UnityEngine;
 using System.Collections;
 
 public class PlayerShootObject : PoolObject {
 	[Range (1,3)]
 	public float speed = 1.0f;
 
+	// Add the tag PlayerShoot to use it in props and other scripts.
+	void Awake (){
+		gameObject.tag = "PlayerShoot";	
+	}
+
 	// Update is called once per frame
 	void Update () {
 		gameObject.transform.Translate(Vector3.forward * speed/5);
 	}
 
-
+	//Cos the enemies have one big sphere collider we ought to verify the distance from enemy position and not the start collision.
 	void OnTriggerStay (Collider other) {
 
 		if (other.CompareTag ("Enemy")) {
---
+++ Assets/Scripts/Props/Batery.cs
@@ -0,0 +1,62 @@
+﻿/* BATERY.CS
+ * (C) COPYRIGHT "JAJA GAMES", 2.016
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * EXPLANATION: 
+ * This script allows the behaviour of Batery prop
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * FUNCTIONS LIST:
+ * 
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * MODIFICATIONS:
+ * DATA			DESCRIPCTION
+ * ----------	-----------------------------------------------------------------------------------------------------------------------
+ * 20/04/2016	GENERIC METHODS TO USE IN SCRIPTS
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class Batery : MonoBehaviour {
+
+	private Animation anim;
+
+	private ParticleSystem smoke;
+	ParticleSystem.EmissionModule emSmoke;
+
+	void Awake () {
+		anim = gameObject.GetComponent<Animation>();
+		smoke = gameObject.GetComponent<ParticleSystem> ();
+		emSmoke = smoke.emission;
+		emSmoke.enabled = false;
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		other.gameObject.SetActive (false);
+
+		if (anim.isPlaying)
+			return;
+		
+		if (smoke.startSize > 1.5f)
+			gameObject.SetActive (false);
+
+		if (other.CompareTag ("PlayerShoot")) {
+			
+			anim.Play ();
+			Invoke ("StopAnim",anim.clip.length);  		//play once time
+			emSmoke.enabled = true;
+
+			if (emSmoke.rate.constantMax < 5) {
+				var rate = emSmoke.rate;
+				rate.constantMax += 1;
+				emSmoke.rate = rate;
+			} else
+				smoke.startSize += 0.1f;
+		}
+	}
+
+	private void StopAnim(){
+		anim.Stop();
+	}
+}
