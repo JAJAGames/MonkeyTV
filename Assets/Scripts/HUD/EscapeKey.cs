90d8c2b1744d93976f631ac7a5ada7e448794653
AntoniGari <slemonboy@hotmail.com>
2016-04-22 12:38:06 +0200

    Exit from a scene
    Pressing Esc you can go to the game menu

---
+++ Assets/Scripts/HUD/EscapeKey.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+#if UNITY_5_3
+using UnityEngine.SceneManagement;
+#endif
+
+public class EscapeKey : MonoBehaviour {
+	
+	private const int MENUID = 0;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+#if UNITY_5_3
+			SceneManager.LoadScene(MENUID);
+#endif
+#if UNITY_5_2
+			Application.LoadLevel(MENUID);
+#endif
+		}
+	}
+}
