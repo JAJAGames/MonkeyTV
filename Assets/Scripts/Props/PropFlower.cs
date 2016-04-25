5f394a2c606616c1c5443ead77ca70572177bd8f
AntoniGari <slemonboy@hotmail.com>
2016-04-25 12:09:06 +0200

    Flower Animation
    Flower is animated when the player touch it

---
+++ Assets/Scripts/Props/PropFlower.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PropFlower : MonoBehaviour {
+
+	private Animation animation;
+
+	void Awake () {
+		animation = gameObject.GetComponent<Animation> ();
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.CompareTag ("Player") || other.CompareTag ("Enemy")) {
+			StartAnimation ();
+		}
+	}
+
+	void OnTriggerExit (Collider other){
+		if (other.CompareTag ("Player") || other.CompareTag ("Enemy")) {
+			StartAnimation ();
+		}
+	}
+
+	private void StartAnimation() {
+		if (animation.isPlaying) {
+			return;
+		}
+		animation.Play ();
+		Invoke ("StopAnim",animation.clip.length); 
+	}
+
+	private void StopAnim(){
+		animation.Stop();
+	}
+}
