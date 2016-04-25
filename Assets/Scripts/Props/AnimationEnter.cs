e77884bf46cc15e7f4c9ca06809b824ea24d3314
Jesús Mosquera Mayo <jesusmm12@TTC.CAT>
2016-04-25 14:10:56 +0200

    saved secenes
    gorilla updated
    updoor updated
    cube with legacy animation

---
+++ Assets/Scripts/Props/AnimationEnter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationEnter : MonoBehaviour {
+
+	private Animation animation;
+	void Awake () {
+		animation = gameObject.GetComponent<Animation> ();
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.CompareTag ("Player")) {
+			StartAnimation ();
+		}
+	}
+		
+
+	private void StartAnimation() {
+		if (animation.isPlaying) {
+			return;
+		}
+		animation.Play ();
+		Invoke ("StopAnim",animation.clip.length); 
+	}
+
+	private void StopAnim(){
+		animation.Stop();
+		animation.enabled = false;
+		this.enabled = false;
+	}
+}
