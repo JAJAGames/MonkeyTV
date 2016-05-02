9e98ad6ed60094833dde66763bd52d2270ae00a0
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-05-02 10:46:49 +0200

    camera follow
    camera always following player.

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -27,44 +27,31 @@ public class CameraFollow : MonoBehaviour {
 
 	private Vector3 initDifference;
 	private float smooth;
-	private bool playerVisible;
-    private float timer;                //minimum waiting time between combo jumps.
 	//NEVER FOLLOW PLAYER JUMPING
 
 	private void Start() 
 	{
 		initDifference = transform.position - playerMove.transform.position; 				//get the initial offset
 		smooth = playerMove.gameObject.GetComponent<PlayerMovement>().movementSpeed * 3;	//get the speed of camera. It should be faster than player
-		playerVisible = true;                                                               //in initial offset athe player is visible
     }
 
-	private void Update() {
+	private void Update() 
+	{
 		 
-		//get the position of player in screen
-		Vector3 screenPoint = Camera.main.WorldToViewportPoint (playerMove.transform.position); 
-
-		//is visible?
-		if (screenPoint.z > 0.1 && screenPoint.x > 0.1 && screenPoint.x < 0.9 && screenPoint.y > 0.1 && screenPoint.y < 0.9)
-			playerVisible = true;
-		else
-			playerVisible = false;
-
-        //visible and jumping the camera remains in same place --> stop update
-        if (!playerMove.controller.isGrounded)
-        {
-            timer = 0.1f;                                                                   //on air reset timer
-            if (playerVisible)
-                return;
-        }
-		// current offset
 		Vector3 difference = transform.position - playerMove.transform.position;
-
-        timer -= Time.deltaTime;
-        //player visible and not jumping -> move camera.
-        if (difference != initDifference && playerVisible && timer < 0)
-            transform.position = Vector3.MoveTowards(transform.position, playerMove.transform.position + initDifference, smooth * Time.deltaTime);
-        //is out of screen set position with initial offset to camera
-        if (!playerVisible)
-			transform.position =  playerMove.transform.position + initDifference;
+        
+		//player visible and not jumping -> move camera.
+		if (difference != initDifference) 
+		{
+			if (playerMove.controller.isGrounded)
+				transform.position = Vector3.MoveTowards (transform.position, playerMove.transform.position + initDifference, smooth * Time.deltaTime);
+			else 
+			{
+				difference = playerMove.transform.position + initDifference;
+				difference.y = transform.position.y;
+				transform.position = Vector3.MoveTowards (transform.position, difference, smooth * Time.deltaTime);
+			}
+		}
 	}
+	
 }
