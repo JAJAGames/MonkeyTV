61d7408cfd8f9fe034d91956549530d4ceacca47
AntoniGari <slemonboy@hotmail.com>
2016-05-03 16:49:42 +0200

    Pick Items done
    The player is able to pick items

---
+++ Assets/Scripts/Player/PickItems.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickItems : MonoBehaviour {
+
+	public enum itemsListMasterChef {ITEM_TOMATO=0, ITEM_BANANA=1, NO_ITEM=2};
+
+	public itemsListMasterChef actualItem;
+
+	[Tooltip("Items")]
+	public Transform Items;
+	public GameObject[] allItems;
+
+	// Use this for initialization
+	void Start () {
+		actualItem = itemsListMasterChef.NO_ITEM;
+
+		allItems = HelperMethods.GetChildren (Items);
+		for (int i = 0; i < allItems.Length; i++) 
+			allItems[i].GetComponent<MeshRenderer> ().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Throw") && actualItem != itemsListMasterChef.NO_ITEM) {
+			throwItem ();
+		}
+	}
+
+	void change() {
+	
+	}
+
+	public void throwItem() {
+		//Throw item animation
+		allItems[(int)actualItem].GetComponent<MeshRenderer> ().enabled = false;
+		actualItem = itemsListMasterChef.NO_ITEM;
+	}
+
+	public bool haveItem() {
+		return actualItem == itemsListMasterChef.NO_ITEM;
+	}
+
+	public void changeItem(itemsListMasterChef newItem) {
+		actualItem = newItem;
+		allItems[(int)newItem].GetComponent<MeshRenderer> ().enabled = true;
+	}
+}
---
+++ Assets/Scripts/Props/PropPickItem.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PropPickItem : MonoBehaviour {
+
+	public PickItems.itemsListMasterChef itemType;
+	public MeshRenderer meshRenderer;
+	public SphereCollider sphereCollider;
+
+	public PickItems player;
+
+	// Use this for initialization
+	void Start () {
+		meshRenderer = GetComponent<MeshRenderer> ();
+		sphereCollider = GetComponent<SphereCollider> ();
+		player = GameObject.FindWithTag ("Player").GetComponent<PickItems> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay (Collider other){
+
+		if (other.CompareTag ("Player") && Input.GetButtonDown ("Pick")) {
+			if (player.haveItem()) {
+				player.changeItem(itemType);
+				StartCoroutine (Respawn ());
+			}
+		}
+
+	}
+
+	private IEnumerator Respawn() {
+		meshRenderer.enabled = false;
+		sphereCollider.enabled = false;
+		yield return new WaitForSeconds (5.0f);
+
+		//Respawn animation
+
+		meshRenderer.enabled = true;
+		sphereCollider.enabled = true;
+	}
+}
