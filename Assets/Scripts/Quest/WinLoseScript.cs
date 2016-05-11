1a7bd11cce68ec3e1076e779b83e6efa37b135d2
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-05-11 10:46:05 +0200

    win lose text on screen
    saved main and jesus scenes

--- Assets/Scripts/Quest/WinLoseScript.cs
+++ Assets/Scripts/Quest/WinLoseScript.cs
@@ -20,13 +20,16 @@
 
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class WinLoseScript : MonoBehaviour {
 
+	public Text text;
 	Animator anim;
 	// Use this for initialization
 	void Awake () {
 		anim = GetComponent<Animator> ();
+		text.gameObject.SetActive (false);
 	}
 	
 	// Update is called once per frame
@@ -40,6 +43,8 @@ public class WinLoseScript : MonoBehaviour {
 	private IEnumerator LoseTheGame (float t) {
 		
 		anim.SetTrigger("Dead");												//activate death animation 
+		text.text = "GAME OVER";
+		text.gameObject.SetActive(true);
 		gamestate.Instance.SetState (Enums.state.STATE_INIT);
 		yield return new WaitForSeconds (t);
 
@@ -49,44 +54,12 @@ public class WinLoseScript : MonoBehaviour {
 	private IEnumerator WinTheGame(float t) {
 		
 		anim.SetTrigger("Win");
+		text.text = "YOU WIN!";
+		text.gameObject.SetActive(true);
 		gamestate.Instance.SetState (Enums.state.STATE_INIT);
 		yield return new WaitForSeconds(t);
 		gamestate.Instance.SetLevel (0);
 
 	}
 		
-
-	//OnGUI deprecatet and efficience low --> We Must remove this in future
-	void OnGUI() {
-		
-		if (gamestate.Instance.GetState () == Enums.state.STATE_WIN) {
-			//Text properties
-			int w = Screen.width, h = Screen.height;
-
-			GUIStyle style = new GUIStyle ();
-			style.alignment = TextAnchor.UpperCenter;
-			style.fontSize = h / 10;
-			style.fontStyle = FontStyle.Bold;
-			style.normal.textColor = Color.green;
-
-			Rect rect = new Rect (w / 2 - 50, h / 2 - 25, 100, 50);
-
-			GUI.Label (rect, "YOU WIN!", style);
-		}
-
-		if (gamestate.Instance.GetState () == Enums.state.STATE_LOOSE) {
-			//Text properties
-			int w = Screen.width, h = Screen.height;
-
-			GUIStyle style = new GUIStyle ();
-			style.alignment = TextAnchor.UpperCenter;
-			style.fontSize = h / 10;
-			style.fontStyle = FontStyle.Bold;
-			style.normal.textColor = Color.green;
-
-			Rect rect = new Rect (w / 2 - 50, h / 2 - 25, 100, 50);
-
-			GUI.Label (rect, "GAME OVER", style);
-		}
-	}
 }
