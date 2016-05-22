f0d6cc28ab78dadcfe9a1944bc6290452105dbfe
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-05-22 16:49:36 +0200

    menu animations
    main menu animations added

---
+++ Assets/ButtonAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonAnimation: MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	public bool isOver = false;
+	private Animator anim;
+
+	void Awake(){
+		anim = GetComponent<Animator> ();
+		anim.SetBool ("OnOver",false);
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		anim.SetBool ("OnOver",true);
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		anim.SetBool ("OnOver",false);
+	}
+}
