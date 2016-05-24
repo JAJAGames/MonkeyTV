f40a5df882d62b27c8e1e2534faf133c69c1c890
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-05-24 17:02:31 +0200

    HUD changes
    FromWorldToIGU get the canvas size
    then change anchor point
    and now can set the correct position of elements in ui

--- Assets/Scripts/HUD/IGUfromWorld.cs
+++ Assets/Scripts/HUD/IGUfromWorld.cs
@@ -13,10 +13,14 @@ public class IGUfromWorld : MonoBehaviour {
 	private Image img;
 	private bool animate;
 
-
 	void Awake () {
 		img = GetComponent<Image> ();
 		animate = false;
+		Vector3 tmpPos = img.rectTransform.localPosition;
+		img.rectTransform.anchorMin = Vector2.zero;
+		img.rectTransform.anchorMax = Vector2.zero;
+		tmpPos.y = transform.parent.GetComponent<Canvas> ().pixelRect.height + tmpPos.y;
+		img.rectTransform.localPosition = tmpPos;
 	}
 
 	void Update(){
@@ -25,20 +29,17 @@ public class IGUfromWorld : MonoBehaviour {
 
 			if (alpha > 0)
 				alpha -= Time.deltaTime * speedAnimation;
-
 			else {
 				alpha = 0;
 				animate = false;
 			}
-			Vector3 final = HUDPoint *(1-alpha) +new Vector3(Camera.main.WorldToScreenPoint (target.position).x ,Camera.main.WorldToScreenPoint (target.position).y,0) * alpha  ;
+			Vector3 final = HUDPoint * (1 - alpha) + new Vector3 (Camera.main.WorldToScreenPoint (target.position).x, Camera.main.WorldToScreenPoint (target.position).y, 0) * alpha;
 			img.rectTransform.anchoredPosition = final;
-		}
-
-
+		} 
 	}
 
 	public void StartAnimation(){
-		
+
 		HUDPoint = img.rectTransform.anchoredPosition;					//gameobject must be child of canvas and the anchorpoint only can be set on the center.
 		alpha = 1f;
 		animate = true;
