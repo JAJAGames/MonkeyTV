6587aef6f3d5c84718c594181214d224e19a8c01
Jesús Mosquera Mayo <jesusmm12@TTC.CAT>
2016-05-26 11:13:01 +0200

    arrow in cooking pot
    new arrow in cooking pot

--- Assets/Scripts/Props/ShowHideArrow.cs
+++ Assets/Scripts/Props/ShowHideArrow.cs
@@ -10,12 +10,14 @@ public class ShowHideArrow : MonoBehaviour {
 	void Awake () {
 		player = GameObject.Find ("Player").GetComponent<PickItems>();
 		mesh = GetComponent<MeshRenderer> ();
-		child = transform.GetChild (0);
+		if (transform.childCount > 0)
+			child = transform.GetChild (0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		mesh.enabled = (player.actualItem != Enums.itemsListMC.NO_ITEM);
-		child.gameObject.SetActive (player.actualItem != Enums.itemsListMC.NO_ITEM);
+		if (child)
+			child.gameObject.SetActive (player.actualItem != Enums.itemsListMC.NO_ITEM);
 	}
 }
