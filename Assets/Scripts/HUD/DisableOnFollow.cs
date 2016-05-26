d8ae456e527dd1735cea3bae0831bf3f84e00735
Jesús Mosquera Mayo <jesusmm12@TTC.CAT>
2016-05-26 10:15:03 +0200

    controls
    Added to hud keyboard controls

---
+++ Assets/Scripts/HUD/DisableOnFollow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableOnFollow : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (gamestate.Instance.GetState () == Enums.state.STATE_CAMERA_FOLLOW_PLAYER)
+			gameObject.SetActive (false);
+	}
+}
