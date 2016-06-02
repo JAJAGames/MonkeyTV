01a48556d768fdee1b28a37bf6c1e760329a555d
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-06-02 12:01:43 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Assets/Scenes/Release/Level1MasterChef.unity

---
+++ Assets/Scripts/Props/PlacardFx.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlacardFx : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if (!AudioManager.Instance.FxIsPlaying() && (other.CompareTag("Player")||other.CompareTag("Enemy")))
+			AudioManager.Instance.PlayFX (Enums.fxClip.PLACARD_COLLISION);
+	}
+}
