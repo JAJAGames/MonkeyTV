7e74a750ae5941065e31630ff923776817d9d3f6
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-06-02 12:10:22 +0200

    script placard modified
    script placard modified

--- Assets/Scripts/Props/PlacardFx.cs
+++ Assets/Scripts/Props/PlacardFx.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlacardFx : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other){
-		if (!AudioManager.Instance.FxIsPlaying() && (other.CompareTag("Player")||other.CompareTag("Enemy")))
+		if (other.CompareTag("Player")||other.CompareTag("Enemy"))
 			AudioManager.Instance.PlayFX (Enums.fxClip.PLACARD_COLLISION);
 	}
 }
