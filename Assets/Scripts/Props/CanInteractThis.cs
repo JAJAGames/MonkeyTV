dcb39c133735e7f84fca0b4535889fc44af8ea1a
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-06-13 13:38:06 +0200

    more about feedback
    flowers cubes and placards

---
+++ Assets/Scripts/Props/CanInteractThis.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanInteractThis : MonoBehaviour {
+
+	public MeshRenderer mesh1, mesh2;
+	public SkinnedMeshRenderer skinned;
+	private Color color1,color2, colorS;
+
+	public bool showFeedback = true;
+
+	void Awake(){
+		if (mesh1)
+			color1 = mesh1.material.GetColor("_Color");
+		if (mesh2)
+			color2 = mesh2.material.GetColor("_Color");
+		if (skinned)
+			colorS = skinned.material.GetColor("_Color");
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.CompareTag ("Player")) {
+			if (mesh1)
+				mesh1.material.SetColor ("_Color", Color.white);
+			if (mesh2)
+				mesh2.material.SetColor ("_Color", Color.white);
+			if (skinned)
+				skinned.material.SetColor("_Color", Color.white);
+		}
+	}
+
+	void OnTriggerExit (Collider other){
+		if (other.CompareTag ("Player")) {
+			if (mesh1)
+				mesh1.material.SetColor ("_Color", color1);
+			if (mesh2)
+				mesh2.material.SetColor ("_Color", color2);
+			if (skinned)
+				skinned.material.SetColor("_Color", colorS);
+		}
+	}
+}
