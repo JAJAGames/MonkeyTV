8c37959a9c75f982bc42a2c6bae8cabc9c5575bb
AntoniGari <slemonboy@hotmail.com>
2016-06-21 20:41:20 +0200

    Move while Jump
    Oscar can move while jumping

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -43,7 +43,8 @@ public class PlayerMovement : MonoBehaviour  {
 
 	private void FixedUpdate ()  {
 		
-					
+		var inputDevice = InputManager.ActiveDevice;
+
 		if (gamestate.Instance.GetState () == Enums.state.STATE_LOSE || gamestate.Instance.GetState() == Enums.state.STATE_PLAYER_PAUSED)		//skip update for game Losed 
 			return;
 																			//no update for deads... no Zombies please!
@@ -56,11 +57,11 @@ public class PlayerMovement : MonoBehaviour  {
 				StartCoroutine (ParticlesJump ());
 			}
 
-			var inputDevice = InputManager.ActiveDevice;
 			if (InputManager.ActiveDevice == InputDevice.Null)
 				moveDirection = new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 			else
 				moveDirection = new Vector3 (inputDevice.LeftStickX, 0, inputDevice.LeftStickY);
+			
 			//Make the player face his movement direction. We ought to disable rotation script
 			if (moveDirection != Vector3.zero)							//Look to rotation viewing is not Zero
 				transform.rotation = Quaternion.LookRotation (moveDirection);
@@ -82,10 +83,22 @@ public class PlayerMovement : MonoBehaviour  {
 				anim.SetBool ("Jump", true);
 				moveDirection.y = jumpSpeed;
 				grounded = false;
-
 			} 
 
 		} else {
+
+			float aux = moveDirection.y;
+			if (InputManager.ActiveDevice == InputDevice.Null)
+				moveDirection = new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			else
+				moveDirection = new Vector3 (inputDevice.LeftStickX, 0, inputDevice.LeftStickY);
+
+			if (moveDirection != Vector3.zero)							//Look to rotation viewing is not Zero
+					transform.rotation = Quaternion.LookRotation (moveDirection);
+
+			moveDirection *= movementSpeed;
+			moveDirection.y = aux;
+								
 			grounded = false;
 		}
 
@@ -117,8 +130,6 @@ public class PlayerMovement : MonoBehaviour  {
 	}
 
 	public void AddForce(Vector3 force){
-		
 		externalForce += force;
 	}
-		
 }
