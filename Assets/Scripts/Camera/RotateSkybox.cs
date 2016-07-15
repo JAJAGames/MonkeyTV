0c0fa3bbcda0a3186661d38301eb88557c2a6069
AntoniGari <slemonboy@hotmail.com>
2016-07-15 19:18:43 +0200

    Level Star Wars preparation
    - Added all the level, with colliders
    - Added a skybox
    - The skybox rotates

---
+++ Assets/Scripts/Camera/RotateSkybox.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateSkybox : MonoBehaviour {
+
+	private float scrollSpeed;
+	private float horizontalOffset;
+
+	// Use this for initialization
+	void Start () {
+		scrollSpeed = 2.0f;
+		horizontalOffset = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Set Horizontal Offset
+		horizontalOffset = horizontalOffset + (scrollSpeed * Time.deltaTime);
+
+		//Animate Texture.
+		RenderSettings.skybox.SetFloat ("_Rotation", horizontalOffset);
+	}
+}
