381d901a259db2d470309a21eb08e22c8ba5fd45
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-07-18 10:45:00 +0200

    npc guide
    npc guide added on monkey wars

---
+++ Assets/Scripts/NPCS/R2D2/R2D2Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class R2D2Controller : MonoBehaviour {
+
+	private bool _quest = false;
+	// Update is called once per frame
+
+	void Update () {
+		if (_quest)
+			return;
+		
+		if (gamestate.Instance.GetState () == Enums.state.STATE_INIT)
+			gamestate.Instance.SetState(Enums.state.STATE_STATIC_CAMERA);
+
+		Invoke("QuesToFalse", 3);
+	}
+
+	void QuesToFalse(){
+		_quest = false;
+		gamestate.Instance.SetState(Enums.state.STATE_CAMERA_FOLLOW_PLAYER);
+	}
+
+}
