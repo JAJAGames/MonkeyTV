954a0e3fc0ffb9238d3f82a7877fd3f6f523b2eb
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-09-17 14:02:38 +0200

    Revert "all boss done erased"
    This reverts commit 76695a3c6ff0feef567d295daa156312ee1a04f9.

---
+++ Assets/Scripts/Quest/PanelControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using Enums;
+using InControl;
+public class PanelControl : MonoBehaviour {
+
+	public StatePatternBoss boss;
+	private Transform button;
+	// Use this for initialization
+	void Awake () {
+		boss = GameObject.Find ("Boss").GetComponent<StatePatternBoss> ();
+		button = transform.FindChild ("Canvas");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (boss.actualState == BossState.PUNCH_STATE) 
+			button.gameObject.SetActive (true);
+		else
+			button.gameObject.SetActive (false);
+	
+	}
+
+	public void OnTriggerStay (Collider other) {
+		if (boss.actualState != BossState.PUNCH_STATE)
+			return;
+		var inputDevice = InputManager.ActiveDevice;
+		if (other.gameObject.CompareTag ("Player")) {
+			if ( Input.GetButton("Pick") || inputDevice.Action3 ){ //inputDevice.Action3 or pickNutton
+				boss.batteries--;
+				gameObject.SetActive (false);
+			}
+		}
+	}
+}
