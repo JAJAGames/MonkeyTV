bb6b3715d785d785a2efb54a220ce0de737d44ae
Jesús Mosquera Mayo <qweilak@gmail.com>
2016-09-27 13:09:41 +0200

    monkeycheff
    popup shows on first meeting with jailden monkeys

--- Assets/Scripts/HUD/PopUp.cs
+++ Assets/Scripts/HUD/PopUp.cs
@@ -6,15 +6,25 @@ public class PopUp : MonoBehaviour {
 
 	private DataRead _text;
 	private Animator _anim;
+	private bool once;
 	// Use this for initialization
 	void Awake () {
 		_text = transform.FindChild ("Text").GetComponent <DataRead>();
 		_anim = GetComponent<Animator> ();
+		once = false;
 	}
 	
 	// Update is called once per frame
 	public void ShowPopUp (int code) {
 		_text.ChangeText (code);
 		_anim.SetTrigger ("Show");
+	}
+
+	public void ShowPopUpOnce (int code) {
+		if (once)
+			return;
+		_text.ChangeText (code);
+		_anim.SetTrigger ("Show");
+		once = true;
 	}
 }
---
+++ Assets/Scripts/Props/PopUpShowOnEnable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopUpShowOnEnable : MonoBehaviour {
+
+	public PopUp popup; 
+
+	void OnEnable() {
+		if (gamestate.Instance.GetState() == Enums.state.STATE_CAMERA_FOLLOW_PLAYER)
+			popup.ShowPopUpOnce(18);
+	}
+}
